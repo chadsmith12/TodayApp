4d9b8bb270c1d1bc3ff8ed88c239652919e985ea
Chad Smith <smith.chad12@gmail.com>
2022-02-05 20:04:18 -0600

    Initial Commit

---
+++ TodayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodayApp
+//
+//  Created by Chad Smith on 2/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
